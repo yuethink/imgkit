19b4fe7cbfc415aa0a4f5b91ce7cb7d10252a421
yuwei <727955957@qq.com>
2026-01-04 03:43:25 +0800

    feat: add vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
   <UApp>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <Analytics />
   </UApp>
 </template>
